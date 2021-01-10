c196aa716d156d3cd2645089ab3371a9e308e019
ggstoltz <gunnar.b.stoltz@gmail.com>
2021-01-10 14:55:24 -0600

    functionality
    Ran application and test

--- Dev/app.js
+++ Dev/app.js
@@ -163,7 +163,7 @@ async function run() {
                             integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" 
                             crossorigin="anonymous">
                     
-                        <title>Team</title>
+                        <title>Document</title>
 
                         <style>
                         .row {
